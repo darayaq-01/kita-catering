75322201284ba7e07e7075d4aa103c9129fec9fd
darayaq-01 <daniel@darayaq.de>
2021-03-03 15:18:53 +0100

    core: cleaning

---
+++ client/src/components/Dashboard/UserData.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserData = () => {
+    return (
+        <div>
+            <h1>users info</h1>
+        </div>
+    );
+};
+
+export default UserData;
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,33 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import UserService from '../../redux/services/user.service';
+import React from 'react';
+
+import Template from '../Dashboard/Template';
 
 import Content from '../Main/Content';
 import Hero from './Hero';
 
 const Home = () => {
-    const [content, setContent] = useState('');
-    /* 
-    useEffect(() => {
-        UserService.getPublicContent().then(
-            (response) => {
-                setContent(response.data);
-            },
-            (error) => {
-                const _content =
-                    (error.response && error.response.data) ||
-                    error.message ||
-                    error.toString();
-
-                setContent(_content);
-            }
-        );
-    }, []);
- */
     return (
         <div>
-            <h3 className="bg-red-400">{content}</h3>
             <Hero />
             <Content />
+            <Template />
         </div>
     );
 };
