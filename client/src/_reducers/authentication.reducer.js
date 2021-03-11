a3e8df987e964eed54751c93b6294ba294a83cf0
darayaq-01 <daniel@darayaq.de>
2021-03-11 11:53:34 +0100

    delete

--- client/src/_reducers/authentication.reducer.js
+++ client/src/_reducers/authentication.reducer.js
@@ -1,12 +1,13 @@
 import { userConstants } from '../_constants/user.constants';
 
-
-let user = localStorage.getItem('user') !== undefined ? JSON.parse(localStorage.getItem('user')) : null;
-
+let user = JSON.parse(localStorage.getItem('user'));
 const initialState = user ? { loggedIn: true, user } : {};
 
+
 export function authentication(state = initialState, action) {
-    console.log(localStorage.getItem('user'));
+
+    // console.log(localStorage.getItem('user'));
+
     switch (action.type) {
         case userConstants.LOGIN_REQUEST:
             return {
